12fd0a9e6ce948a56aa36bfc23c3423f24865936
TvoyBatyA1234 <maxdem06@gmail.com>
2024-10-18 11:26:18 +0300

    Add all files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "matrix.hpp"
+int main()
+{
+  size_t m = 0;
+  size_t n = 0;
+  int ** t = matrix(m, n);
+  std::cin >> m >> n;
+  std::cout << m << " " << n;
+  std::cout << '\n';
+  clear(t, m);
+}
