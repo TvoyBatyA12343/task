be3839ec6d9b23c581599ddba1ec293d987abc82
TvoyBatyA1234 <maxdem06@gmail.com>
2024-10-18 10:53:35 +0300

    add matrix.hpp

---
+++ matrix.hpp
@@ -0,0 +1,7 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+int** martix(size_t m, size_t n);
+void clear(int** t, size_t m);
+void enter(int** t, size_t m, size_t n);
+void out(const int* const* t, size_t m, size_t n);
+#endif
