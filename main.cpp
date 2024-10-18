5bb126ce42e118066dd5873d6e0e9763db277efc
TvoyBatyA1234 <maxdem06@gmail.com>
2024-10-18 18:03:00 +0300

    Add input check

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@ int main()
 {
   size_t m = 0, n = 0;
   std::cin >> m >> n;
+  if(!std::cin){
+    std::cerr << "Input Error!\n";
+    return 1;
+  }
   int** table = nullptr;
   try{
     table = create(m, n);
+    if(!std::cin){
+      std::cerr << "Input Error!\n";
+    }
   } catch (const std::bad_alloc & e){
     std::cerr << "Out of memory\n";
     return 1;
